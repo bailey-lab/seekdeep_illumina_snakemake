d09db41bb0faa0ca162863dec89cd19dfe2f3c87
Alfred Simkin <alfred.simkin@gmail.com>
2023-07-08 03:25:53 +0000

    added scripts folder

---
+++ scripts/prep_process_cluster.py
@@ -0,0 +1,12 @@
+import subprocess
+process_cluster_commands=snakemake.input['process_cluster_commands']
+output_folder=snakemake.params['output_folder']
+subprocess.call(f'mkdir -p {output_folder}', shell=True)
+for line in open(process_cluster_commands):
+	print(line)
+	split_line=line.strip().split()
+	sample_index=split_line.index('--experimentName')+1
+	sample=split_line[sample_index]
+	output_file=open(output_folder+f'/{sample}_process_cluster_command.sh', 'w')
+	output_file.write(line)
+	print(sample)
