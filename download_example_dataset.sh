ddbc33d952e0ab7de7ab6b3491da1da2d36129af
Alfred Simkin <alfred.simkin@gmail.com>
2025-01-23 17:33:11 +0000

    adding example dataset plus download script

---
+++ download_example_dataset.sh
@@ -0,0 +1,5 @@
+wget https://seekdeep.brown.edu/programs/elucidator.sif
+wget https://baileylab.brown.edu/SeekDeep/data/SeekDeepTutorialData/pf_SMARRT_example/pfsmarrt_example.tar.gz
+tar -xvzf pfsmarrt_example.tar.gz
+wget https://baileylab.brown.edu/SeekDeep/data/plasmodiumData/genomes/pf.tar.gz
+tar -xvzf pf.tar.gz
